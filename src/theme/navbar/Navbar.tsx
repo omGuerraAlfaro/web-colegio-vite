d8a1b44fad2e734ac6761dba8bca884065623312
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2023-09-21 09:59:11 -0300

    ...

--- src/theme/navbar/Navbar.tsx
+++ src/theme/navbar/Navbar.tsx
@@ -1,33 +1,35 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
 import { Navbar as RBNavbar, Nav, Container } from 'react-bootstrap';
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 import AndesLogo from '../../assets/img/LOGOCOLEGIO.png';
 import AndesSlogan from '../../assets/img/slogan2.png';
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const isActive = (path:any) => {
+    return location.pathname === path;
+  };
+
   return (
     <>
       <div className="container">
         <img src={AndesLogo} width="120" height="150" className="my-3" />
         <img src={AndesSlogan} width="210" height="140" className="my-3" />
-
       </div>
       <RBNavbar bg="light" expand="lg" id='color-bg' className='mb-4 shadow'>
         <Container>
-          {/* <RBNavbar.Brand href="#">
-          </RBNavbar.Brand> */}
           <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
           <RBNavbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto d-flex justify-content-between align-items-center w-100">
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/">Inicio</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/comunidad">Nuestra Comunidad</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/pae">Programa PAE</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/utiles">Lista de Útiles</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/reglamento">Reglamento interno de Convivencia Escolar</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/talleres">Talleres</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/contacto">Contacto</Nav.Link>
-              <Nav.Link className="text-wrap custom-link text-light bold" href="/admision">Admisión</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/") ? "active-link" : ""}`} href="/">Inicio</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/comunidad") ? "active-link" : ""}`} href="/comunidad">Nuestra Comunidad</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/pae") ? "active-link" : ""}`} href="/pae">Programa PAE</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/utiles") ? "active-link" : ""}`} href="/utiles">Lista de Útiles</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/reglamento") ? "active-link" : ""}`} href="/reglamento">Reglamento interno de Convivencia Escolar</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/talleres") ? "active-link" : ""}`} href="/talleres">Talleres</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/contacto") ? "active-link" : ""}`} href="/contacto">Contacto</Nav.Link>
+              <Nav.Link className={`text-wrap custom-link text-light bold ${isActive("/admision") ? "active-link" : ""}`} href="/admision">Admisión</Nav.Link>
             </Nav>
           </RBNavbar.Collapse>
         </Container>
